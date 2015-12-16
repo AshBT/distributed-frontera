e21442257c2a91f8f3cae359318dd1165cdf5feb
Alexander Sibiryakov <sixty-one@yandex.ru>
2015-12-16 18:39:33 +0100

    rem canonical solver, states from frontera

--- distributed_frontera/worker/strategy/bfs.py
+++ distributed_frontera/worker/strategy/bfs.py
@@ -1,37 +1,30 @@
 # -*- coding: utf-8 -*-
 from urlparse import urlparse
-from frontera.contrib.canonicalsolvers.basic import BasicCanonicalSolver
-from distributed_frontera.backends.hbase import _state
+from frontera.core.components import States
 from base import BaseCrawlingStrategy
 
 
 class CrawlingStrategy(BaseCrawlingStrategy):
-    def __init__(self):
-        self.canonicalsolver = BasicCanonicalSolver()
-
     def add_seeds(self, seeds):
         scores = {}
         for seed in seeds:
             if seed.meta['state'] is None:
-                url, fingerprint, _ = self.canonicalsolver.get_canonical_url(seed)
-                scores[fingerprint] = 1.0
-                seed.meta['state'] = _state.get_id('QUEUED')
+                scores[seed.meta['fingerprint']] = 1.0
+                seed.meta['state'] = States.QUEUED
         return scores
 
     def page_crawled(self, response, links):
         scores = {}
-        response.meta['state'] = _state.get_id('CRAWLED')
+        response.meta['state'] = States.CRAWLED
         for link in links:
             if link.meta['state'] is None:
-                url, fingerprint, _ = self.canonicalsolver.get_canonical_url(link)
-                scores[fingerprint] = self.get_score(url)
-                link.meta['state'] = _state.get_id('QUEUED')
+                scores[link.meta['fingerprint']] = self.get_score(link.url)
+                link.meta['state'] = States.QUEUED
         return scores
 
     def page_error(self, request, error):
-        url, fingerprint, _ = self.canonicalsolver.get_canonical_url(request)
-        request.meta['state'] = _state.get_id('ERROR')
-        return {fingerprint: 0.0}
+        request.meta['state'] = States.ERROR
+        return {request.meta['fingerprint']: 0.0}
 
     def get_score(self, url):
         url_parts = urlparse(url)
